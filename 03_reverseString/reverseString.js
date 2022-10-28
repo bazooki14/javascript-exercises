8a1f58a8d4d7818f4d301cc65faaa92670254c42
bazooki14 <brandonbouska14@gmail.com>
2022-10-28 16:23:51 -0700

    Complete exercises 01 - 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let returnString = '';
+    let i = 1;
+    while (i <= string.length) {
+        returnString += string.charAt(string.length - i);
+        i++;
+    };
+    return returnString;
 };
 
 // Do not edit below this line
