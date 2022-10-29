4293e3855775de8ebc073b40bb508bfff7351ee2
bazooki14 <brandonbouska14@gmail.com>
2022-10-29 15:26:12 -0700

    Passed two of five tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    let argArray = [...toRemove]
+    let cleanArray = array.filter(cleaner);
+    function cleaner(testee) {
+        for (let i = 0; i < argArray.length; i++) {
+            if (testee === argArray[i]) {continue}
+            else {
+                return testee;
+            }
+        }
+    }
+    return cleanArray;
 };
 
 // Do not edit below this line
