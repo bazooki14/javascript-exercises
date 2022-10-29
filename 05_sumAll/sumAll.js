34fe4486899975f03aa9964b312a0dbb85d44b6a
bazooki14 <brandonbouska14@gmail.com>
2022-10-29 16:35:09 -0700

    All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let sum;
+    if (first > last) {
+        let initial = first;
+        let final = last;
+        for (let i = initial; i <= final; i++) {
+            sum += i;
+        };
+    } else if (first < last) {
+        let initial = last;
+        let final = first;
+        for (let i = initial; i <= final; i++) {
+            sum += i;
+        };
+    } else if (first < 0 || last < 0 || first.isNaN() || last.isNaN() ) {
+        sum = 'ERROR';
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
