8a967f12af4e53ce79c5bdf18b0a3ad10db5c74b
bazooki14 <brandonbouska14@gmail.com>
2022-10-30 09:14:26 -0700

    Passed all tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,20 +1,10 @@
 const ftoc = function(input) {
-  let output;
-  if (input < 0) {
-    output = Math.round((((Math.abs(input) - 32) * (5 / 9)) * -1) * 10) / 10;
-  } else {
-    output = Math.round(((input - 32) * (5 / 9)) * 10) / 10;
-  }
+  let output = Math.round(((input - 32) * (5 / 9)) * 10) / 10;
   return output;
 };
 
 const ctof = function(input) {
-  let output;
-  if (input < 0) {
-    output = Math.round(((Math.abs(input) * (9 / 5) + 32) * -1) * 10) / 10;
-  } else {
-    output = Math.round((input * (9 / 5) + 32) * 10) / 10;
-  }
+  let output = Math.round((input * (9 / 5) + 32) * 10) / 10;
   return output;
 };
 
