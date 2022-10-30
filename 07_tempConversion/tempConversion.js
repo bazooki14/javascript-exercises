28241ca23d63a00c6604051df8858b26510089af
bazooki14 <brandonbouska14@gmail.com>
2022-10-30 09:12:15 -0700

    Passed all tests except negative numbers

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  let output;
+  if (input < 0) {
+    output = Math.round((((Math.abs(input) - 32) * (5 / 9)) * -1) * 10) / 10;
+  } else {
+    output = Math.round(((input - 32) * (5 / 9)) * 10) / 10;
+  }
+  return output;
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let output;
+  if (input < 0) {
+    output = Math.round(((Math.abs(input) * (9 / 5) + 32) * -1) * 10) / 10;
+  } else {
+    output = Math.round((input * (9 / 5) + 32) * 10) / 10;
+  }
+  return output;
 };
 
 // Do not edit below this line
