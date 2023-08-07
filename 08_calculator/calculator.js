9a0c09f488bafd55e3d3cf3c63b9d5990543027e
bazooki14 <brandonbouska14@gmail.com>
2023-08-06 21:19:20 -0700

    Completed calculator exercise. All tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
+const add = function(a, b) {
+	return (a + b)
+};
+
+const subtract = function(a, b) {
+	return(a - b)
+};
+
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0)
+};
+
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1)
+};
+
+const power = function(a, b) {
+	return Math.pow(a, b)
+};
+
+const factorial = function(input) {
 	
-};
+  if (input === 0) {
+    return 1;
+  } else {
+  
+  let array = [input];
+  for (let i = input; i > 1; i--) {
+    let addition = (i - 1);
+    
+    array.push(addition);
+  };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+  return array.reduce((product, current) => product * current, 1)  
+}};
 
 // Do not edit below this line
 module.exports = {
