355e76866090b47e0d337bdef60d6a17fc4ee819
bazooki14 <brandonbouska14@gmail.com>
2023-08-12 17:09:01 -0700

    Passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,27 @@
-const palindromes = function () {
+const palindromes = function (maybe) {
+    let may = maybe.trim()
+                   .toLowerCase()
+                   .replaceAll('!', '')
+                   .replaceAll(',', '')
+                   .replaceAll('.', '')
+                   .replaceAll(' ', '');
+    
+    let array = may.split("");
+
+    let revArray = [...array];
+
+    revArray.reverse();
+    
+    let arrayString = array.toString();
+    
+    let revArrayString = revArray.toString();
+
+
+    if (arrayString === revArrayString) {
+        return true;
+    } else {
+        return false;
+    };
 
 };
 
