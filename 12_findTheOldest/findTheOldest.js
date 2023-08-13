8c39ce8447e0d32853154081fe7f7af420093822
bazooki14 <brandonbouska14@gmail.com>
2023-08-12 20:43:27 -0700

    Passed all tests in exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people.sort(function(a, b) {
+        if (a.yearOfDeath == undefined) {
+            a.yearOfDeath = new Date().getFullYear();
+        } else if (b.yearOfDeath == undefined) {
+            b.yearOfDeath = new Date().getFullYear();
+        };
+        
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    });
 
+    console.table({oldest});
+
+    return oldest[0];
 };
 
 // Do not edit below this line
