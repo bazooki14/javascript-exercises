4c59a8dcd22060ff536fb1761399ab04113db3a3
bazooki14 <brandonbouska14@gmail.com>
2023-08-12 18:13:17 -0700

    Passed all tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+    parseInt(place);
 
+    if (place < 0) {
+        return "OOPS"
+    };
+    
+    let fib = [1, 1];
+
+    fibber = fib[fib.length - 2];
+
+    for (let i = 0; i < 26; i++) {
+        fib.push(fib[fib.length - 2] + fib[fib.length - 1]);
+    };
+
+    return fib[place - 1];
 };
 
 // Do not edit below this line
